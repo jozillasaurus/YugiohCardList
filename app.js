c0e22fd10f4daa81f0c8328fa09b9e82cbbd3a96
Josias DeLaTorre <jozillasaurus@joes-mbp.home>
2020-09-24 13:33:50 -0700

    yugioh8

--- app.js
+++ app.js
@@ -13,52 +13,114 @@
 // getInfo()
 // .forEach(element => console.log(element)) line 24
 // https://cors-anywhere.herokuapp.com/
+// part 2
+// const url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
+// const input = document.querySelector('#blank')
+// const button = document.querySelector('#search')
 
+// button.addEventListener('click', async () => {
+//   let userInput = input.value
+//   const response = await axios.get(`${url}?&fname=${userInput}`)
+//   // console.log(response.data.data.forEach(element => console.log(element)))
+//   list(response.data.data)
+// })
+
+// pre-starting a new
+// const cardArray = document.querySelector('.card-list')
+// const cardContainer = document.querySelector('.card-container')
+// const name = document.querySelector('card-name')
+// const list = cards => {
+//   removeCards()
+//   cards.forEach(card => {
+//     // const cardContainer = document.getElementsByClassName('card-container')
+//     // cardContainer.className = 'card-container'
+
+//     // const name = document.querySelector('h3')
+//     name.innerHTML = `${card.name}`
+//     cardContainer.appendChild(name)
+
+//     const image = document.createElement('img')
+//     image.src = `${card.card_images[0].image_url_small}`
+//     cardContainer.appendChild(image)
+
+//     // cardArray.appendChild(cardContainer)
+
+//   });
+// }
+// function removeCards() {
+//   const oldCard = document.querySelector('.card-list')
+//   while (oldCard.lastChild) {
+//     oldCard.removeChild(oldCard.lastChild)
+//   }
+// }
+
+// part 3-steve help
+
+
+//all card list https://db.ygoprodeck.com/api/v7/cardinfo.php//
 const url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
-const input = document.querySelector('#blank')
-const button = document.querySelector('#search')
-
-button.addEventListener('click', async () => {
-  let userInput = input.value
-  const response = await axios.get(`${url}?&fname=${userInput}`)
-  // console.log(response.data.data.forEach(element => console.log(element)))
-  list(response.data.data)
-})
-
-const cardArray = document.querySelector('.card-list')
-
-const list = cards => {
+const searchBar = document.querySelector('form')
+searchBar.addEventListener('submit', getData)
+async function getData(e) {
+  e.preventDefault()
+  const userInput = document.querySelector('#blank').value
+  try {
+    const response = await axios.get(`${url}?&fname=${userInput}`)
+    let data = response.data.data
+    // console.log(data)
+    appendData(data)
+  } catch (error) {
+    console.log(`Error: ${error}`)
+  }
+}
+function appendData(data) {
   removeCards()
-  cards.forEach(card => {
-    const cardContainer = document.createElement('div')
-    cardContainer.className = 'card-container'
+  data.forEach(card => {
+    let cardList = document.querySelector('.card-list')
+    let cardDataContainer = document.createElement('div')
+    let cardTitle = document.createElement('h2')
+    cardTitle.textContent = card.name
+    cardDataContainer.append(cardTitle)
+    let cardImage = document.createElement('img')
+    cardImage.src = card.card_images[0].image_url_small
+    cardDataContainer.append(cardImage)
+    cardList.append(cardDataContainer)
+    //Doesn't help--
+    // let allImg = document.querySelectorAll('img')
+    // allImg.forEach(element => {
+    //   console.log(element)
 
-    const name = document.createElement('h3')
-    name.innerHTML = `${card.name}`
-    cardContainer.appendChild(name)
-
-    const image = document.createElement('img')
-    image.src = `${card.card_images[0].image_url_small}`
-    cardContainer.appendChild(image)
-
-    cardArray.appendChild(cardContainer)
-
-    cardContainer.addEventListener('click', async () => {
-      document.getElementById('div').onclick = function changeContent() {
-        document.image.src = `${card.card_images[0].image_url}`
+    cardImage.addEventListener('click', () => {
+      //Doesn't help--
+      // console.log(card.card_images)
+      // let eachImage = card.card_images
+      // console.log('image',card.card_images[0].image_url)
+      if (cardImage.src != card.card_images[0].image_url) {
+        cardImage.src = card.card_images[0].image_url
+      } else {
+        cardImage.src = card.card_images[0].image_url_small
       }
     })
+    // })
+
+
   });
 }
-
-
+function bigImage(card) {
+  // let imgList = document.querySelectorAll('img')
+  // console.log("line 42", card)
+  // card.src = card.card_images[0].image_url
+}
 function removeCards() {
   const oldCard = document.querySelector('.card-list')
   while (oldCard.lastChild) {
     oldCard.removeChild(oldCard.lastChild)
   }
 }
+// cards.forEach(card => {
+//   name.textContent = `${card.name}`
+//   image.src = `${card.card_images[0].image_url_small}`
+// });
 
-function expandImg() {
 
-}
+
